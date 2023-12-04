1ace7d1696a6e2c5740d48aff8f8d3dc492e1607
Henry Chiu <Henry@gmail.com>
2023-12-04 23:51:07 +0800

    another version1

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ app.use(express.static("public"));
 app.set("view engine", "ejs"); //有加這一行再render的時候就不用打 xx.ejs
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const languages = [
+    { name: "Python", rating: 9.5, popularity: 9.7, trending: "super hot" },
+    { name: "Java", rating: 9.4, popularity: 8.5, trending: "hot" },
+    { name: "C++", rating: 9.2, popularity: 7.7, trending: "hot" },
+    { name: "PHP", rating: 9.0, popularity: 5.7, trending: "decreasing" },
+    { name: "JS", rating: 8.5, popularity: 8.7, trending: "hot" },
+  ];
+  res.render("index", { languages });
 });
 
 app.get("/example", (req, res) => {
